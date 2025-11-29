472a71b6b69095485c91b08e15be070e04b87cad
Taliban Mkristu <m25124@gmail.com>
2025-11-29 06:27:30 -0500

    modified order of build script

--- build.sh
+++ build.sh
@@ -8,9 +8,10 @@ python3 -m pip install -r requirements.txt
 #collect static files
 python3 manage.py collectstatic --no-input
 
-python manage.py createsuperuser --noinput
-
 #apply database migrations
 python3 manage.py makemigrations
 python3 manage.py migrate
+
+python manage.py createsuperuser --noinput
+
 
